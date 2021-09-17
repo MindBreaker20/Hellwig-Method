2ccad08526fe988a9d18c98f971adc39417ee505
Bartłomiej Jamiołkowski <61807667+MindBreaker20@users.noreply.github.com>
2021-09-17 22:23:19 +0200

    Update hellwig_method.py

--- hellwig_method.py
+++ hellwig_method.py
@@ -5,54 +5,54 @@ import statistics
 from itertools import product
 from pandas.core.arrays.sparse import dtype
 
-#Deklaracja używanych danych
-file_name = 'example.xlsx' #nazwa dokumentu Excel o rozszerzeniu xslx
-sheet = 'Arkusz1' #nazwa arkusza z tabelą
+#Declaration of used data
+file_name = 'example.xlsx' #the name of the Excel document with the extension xlsx
+sheet = 'Arkusz1' #name of the sheet with the table
 
-data1  = pd.read_excel(file_name, sheet_name = sheet, header = None, engine = 'openpyxl') #pobrane dane z arkusza przy czym 1 wiersz to nazwy,
-                                                                                          #ponieważ z funkcją read_excel są trudne do pozyskania
-data2  = pd.read_excel(file_name, sheet_name = sheet, engine = 'openpyxl') #faktyczne dane 
+data1  = pd.read_excel(file_name, sheet_name = sheet, header = None, engine = 'openpyxl') #name of the sheet with the table, because with the read_excel function they are difficult to get
+                                                                                          
+data2  = pd.read_excel(file_name, sheet_name = sheet, engine = 'openpyxl') #actual data
 
-y = data1.iloc[0][0] #nazwa zmiennej objasnianej
-var_number = len(data1.loc[0]) #liczba wszystkich zmiennych
-variables = [] #wszystkie zmienne objasniajace spelnaijace Vj > 10%
-var_cor = [] #wszystkie współczynniki korelacji miedzy zmiennymi objaśniajacymi a objaśnianą
-H = [] #integralne wskaźniki integralności informacyjnej
-var_win = [] #najlesze zmienne objaśniające
+y = data1.iloc[0][0] #the name of the target variable
+var_number = len(data1.loc[0]) #number of independent variables
+variables = [] #all variables satisfying requirement Vj> 10%
+var_cor = [] #all correlation coefficients between the explanatory and dependent variables
+H = [] #integral indicators of information integrity
+var_win = [] #the best explanatory variables
 
-#ETAP 1 - wspolczynnik zmiennosci Vj > 10%
+#STAGE 1 - coefficient of variation Vj> 10%
 i = 1
-for c in range(1, var_number):  #dla każdej kolumny poza kolumną 1 ze zienna objaśnianą
-    xi = data1.loc[:,i]   #wszystkie wartosci liczbowe z kolumny
-    xi = xi.loc[1:,]     #nie branie pod uwagę 1 wiersza z wartościami nieliczbowymi (nazwy kolumn)
-    mean = statistics.mean(xi) #średnia zmiennej
-    sd = statistics.stdev(xi)  #odchylenie standardowe zmiennej
-    vj = sd/mean  #współczynnik zmienności
-    atr = data1.iloc[0][i] #atrybut zmiennej, która spełnia warunek Vj > 10%
+for c in range(1, var_number):  #for each column except column 1 with the explained variable
+    xi = data1.loc[:,i]   #all numeric values in the column
+    xi = xi.loc[1:,]     #ignoring 1 row with non-numeric values (column names)
+    mean = statistics.mean(xi) #the mean of the variable
+    sd = statistics.stdev(xi)  #standard deviation of the variable
+    vj = sd/mean  #coefficient of variation
+    atr = data1.iloc[0][i] #attribute of a variable that meets the condition Vj> 10%
     if vj > 0.1:
-        variables.append(atr) #dodawania atrybutów poprawnych zmiennych do listy 
+        variables.append(atr) #adding attributes of valid variables to the list
     i += 1
 
-#ETAP 2 - macierz binarna reprezentująca możliwe kombinacje
-m = len(variables) #m jest liczbą zmiennych objaśniajacych, kolumny w macierzy binarnej
-S = 2**m -1 #liczba kombinacji przepuszczonych zmiennych objaśniających, liczba wierszy macierzy binarnej
-binary_matrix = [i for i in product(range(2), repeat = m)] #dwie linijki kodu generujące dowolną macierz z wszystkimi kombinacjami 0-1
+#STEP 2 - binary matrix representing possible combinations
+m = len(variables) #m is the number of explanatory variables, columns in a binary matrix
+S = 2**m -1 #the number of combinations of the passed explanatory variables, the number of rows of the binary matrix
+binary_matrix = [i for i in product(range(2), repeat = m)] #two lines of code generating any matrix with all combinations 0-1
 binary_matrix = np.array(binary_matrix)
-binary_matrix = np.delete(binary_matrix, 0, axis = 0) #usunięcie 1 wiersza z wygenerowanymi 0
+binary_matrix = np.delete(binary_matrix, 0, axis = 0) #delete 1 row with generated multiple 0
 
-#ETAP 3 - macierz korelacji reprezentująca bezwzgledne korelacje między wszystkimi zmiennymi odniesienie do |rij|
+#STEP 3 - correlation matrix representing absolute correlations between all variables reference to | rij |
 data3 = pd.DataFrame(data2)
-corr_frame = np.absolute(data3.corr()) #wartości bezwglądne współczynników korelacji
-corr_matrix = corr_frame.values #zamiana ramki na macierz
+corr_frame = np.absolute(data3.corr()) #absolute values of the correlation coefficients
+corr_matrix = corr_frame.values #change frame to matrix
 corr_matrix = corr_matrix.round(decimals = 6, out = None)
 
-for i in range(1, m+1): #przekazanie współczynników korelacji między xi a y rj - potrzebne do obliczania h
+for i in range(1, m+1): #passing the correlation coefficients between xi and y rj - needed for computing h
     var_cor.append(corr_matrix[0][i])
 
-corr_matrix = np.delete(corr_matrix, 0, axis = 0) #usuniecie pierwszego wiersza i kolumny z korelacją x i y, ponieważ potem jest                                 
-corr_matrix =np.delete(corr_matrix, 0, axis = 1) #obliczanie sumy rij
+corr_matrix = np.delete(corr_matrix, 0, axis = 0) #removing the first row and column with the x and y correlation, because then there is                                 
+corr_matrix =np.delete(corr_matrix, 0, axis = 1) #calculating sum rij
 
-#ETAP 4 - obliczanie indywidualnych wskaźników pojemności informacyjnej h
+#STEP 4 - calculation of individual indicators of information capacity h
 final_matrix = binary_matrix
 final_matrix = final_matrix.astype('float64') 
 for i in range(0, S):
@@ -62,22 +62,22 @@ for i in range(0, S):
             h = h.round(decimals = 6, out = None)
             final_matrix[i][j] = h
 
-#ETAP 5 - obliczanie integralnych wskaźników pojemnosci informacyjnej
-for i in range(0, S): #kolejne H są dodawane do listy, potem na podstawie pozycji znajdzie się index
-    s = np.sum(final_matrix[i]) #najlepszej kombinacji zmiennych objaśniających
+#STAGE 5 - calculation of integral indicators of information capacity
+for i in range(0, S): #subsequent Hs are added to the list, then index will be found based on the position
+    s = np.sum(final_matrix[i]) #the best combination of explanatory variables
     s = round(s, 6)
     H.append(s)
 
-max_H = max(H) #najwyzsze H
-idx = 0 #poszukiwany index
-while True: #przeszukiwanie listy H w poszukiwaniu pozycji max H
+max_H = max(H) #the highest H
+idx = 0 #index you are looking for
+while True: #searches the H list for max H items
     if H[idx] == max_H:
         break
     idx += 1
 
-#ETAP 6 - znalezienie najlepszej kombinacji i prezentacja wyników
+#STEP 6 - finding the best combination and presenting the results
 binary_matrix = binary_matrix.astype('float64')
-for i in range(0, m): #wypełnianie listy nazwami zmiennych z najlepszej kombinacji
+for i in range(0, m): #filling the list with variable names from the best combination
     if binary_matrix[idx][i] == 1:
         var_win.append(variables[i])
 
